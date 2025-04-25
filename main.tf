39483515a4d2968d6d34b0e421a250462fb47940
Shokhista Mardieva <shokhistamardieva@Shokhistas-MacBook-Air.local>
2025-04-25 19:19:06 -0400

    added files

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "029DA-DevOps24"
+    
+    workspaces {
+        prefix = "network-"
+    }
+  }
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>5.0"
+    }
+  }
+}
+
+provider "aws" {}
+
+module "iam" {
+  source = "./modules/iam"
+   read_only = {
+    default = ["read_only_user_1", "read_only_user_2", "read_only_user_3"]
+   }
+}
---
+++ modules/iam/main_aim.tf
@@ -0,0 +1,22 @@
+variable "read_only" {
+    type = list (string)
+     default = ["read_only_user_1", "read_only_user_2", "read_only_user_3"]
+}
+
+resource "aws_iam_user" "read_only" {
+  for_each = toset(var.read_only)
+  name     = each.value
+}
+resource "aws_iam_group" "read_only" {
+   name = "read_only"
+}
+resource "aws_iam_group_membership" "read" {
+   name = "read_only-group-membership"
+   users = var.read_only
+
+   group = aws_iam_group.read_only.name
+}
+resource "aws_iam_group_policy_attachment" "read_only_policy" {
+   group      = aws_iam_group.read_only.name
+   policy_arn = "arn:aws:iam::aws:policy/AmazonConnectReadOnlyAccess"
+}
