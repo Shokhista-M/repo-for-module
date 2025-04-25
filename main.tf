4a9f918e4c3a56cc18be0e1b4034783fb8fb63ae
Shokhista Mardieva <shokhistamardieva@Shokhistas-MacBook-Air.local>
2025-04-25 19:51:31 -0400

    add billing alert module

--- main.tf
+++ main.tf
@@ -17,6 +17,17 @@ terraform {
 
 provider "aws" {}
 
+module "billing_alert" {
+    source = "binbashar/cost-billing-alarm/aws"
+    create_sns_topic = true
+    aws_env = "029DO-FA24"
+    monthly_billing_threshold = 5
+    currency = "USD"
+}
+output "sns_topic_arn" {
+    value = "${module.billing_alert.sns_topic_arns}"
+}
+
 module "iam" {
   source = "./modules/iam"
    read_only = ["read_only_user_1", "read_only_user_2", "read_only_user_3"]
